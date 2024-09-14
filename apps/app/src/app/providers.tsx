b493ce4f2b2ddeb67a1a6dfd8bd0baa51abb0ae2
TheRohit <rohitmeshram000@gmail.com>
2024-09-14 16:33:45 +0530

    fix default export

--- apps/app/src/app/providers.tsx
+++ apps/app/src/app/providers.tsx
@@ -38,7 +38,7 @@ function getQueryClient() {
   }
 }
 
-export function Providers(props: { children: React.ReactNode }) {
+export default function Providers(props: { children: React.ReactNode }) {
   // NOTE: Avoid useState when initializing the query client if you don't
   //       have a suspense boundary between this and the code that may
   //       suspend because React will throw away the client on the initial
