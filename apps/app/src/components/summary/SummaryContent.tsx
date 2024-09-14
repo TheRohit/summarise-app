80574183ee5dedbd40bb7054beb4c379fadc43b9
TheRohit <rohitmeshram000@gmail.com>
2024-09-14 21:57:35 +0530

    ...

--- apps/app/src/components/summary/SummaryContent.tsx
+++ apps/app/src/components/summary/SummaryContent.tsx
@@ -8,9 +8,7 @@ import InitialContent from "./InitialContent";
 
 const getHostName = () => {
   if (process.env.NODE_ENV === "development") return "http://localhost:3000";
-  return `https://${
-    process.env.NEXT_PUBLIC_VERCEL_URL || process.env.VERCEL_URL
-  }`;
+  return "https://summarise-v1.vercel.app";
 };
 
 export default function SummaryContent({
